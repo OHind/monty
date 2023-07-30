475417c61b94963691fa5c3edb7cf21d1db6b945
OHind <ouchani.hind@gmail.com>
2023-07-30 10:20:50 -0700

    Some changes to correct compiling errors

--- monty.h
+++ monty.h
@@ -1,8 +1,10 @@
 #ifndef MONTY_H
 #define MONTY_H
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
